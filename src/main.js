46b453b89852c935b20d05d5ee77bd153b485f11
Trần Công Thon <thontc82@gmail.com>
2021-03-10 16:05:28 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,33 +1,10 @@
-/*=========================================================================================
-  File Name: main.js
-  Description: main vue(js) file
-  ----------------------------------------------------------------------------------------
-  Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
-  Author: Pixinvent
-  Author URL: http://www.themeforest.net/user/pixinvent
-==========================================================================================*/
+import LoginButton from './components/LoginButton'
 
-
-// import Vue from 'vue'
-// import App from './App.vue'
-
-// i18n
-// import i18n from './i18n/i18n'
-
-import LoginButton from "./components/LoginButton.vue";
-
-/* new Vue({
-  router,
-  store,
-  i18n,
-  render: h => h(App)
-}).$mount('#app')
- */
-
-export default {
-    install(Vue, options) {
-        // Let's register our component globally
-        // https://vuejs.org/v2/guide/components-registration.html
-        Vue.component("login-button", LoginButton);
-    }
-};
+let authLib = {}
+authLib.install = function (Vue, options) {
+    Vue.component('LoginButton', LoginButton)
+}
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(authLib);
+}
+export default authLib
