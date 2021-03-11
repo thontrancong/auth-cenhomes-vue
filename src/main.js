22a564843752a105b03b5b533bf3828ec7029aa9
Trần Công Thon <thontc82@gmail.com>
2021-03-11 11:40:26 +0700

    test

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import AuthButton from './compoments/AuthButton'
 
-/* let authLib1 = {}
+let authLib1 = {}
 authLib1.install = function (Vue, options) {
     Vue.component('AuthButton', AuthButton)
 }
 if (typeof window !== 'undefined' && window.Vue) {
     window.Vue.use(authLib1);
-} */
-export default {
-    AuthButton
 }
+export default authLib1
